ec13ceb4544929b47bcd24e8753447f21f377cea
lni_T <developer.lni@gmail.com>
2016-04-17 19:24:02 +0900

    Fix typo

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -1,10 +1,9 @@
 class LinebotsController < ApplicationController
-  rotect_from_forgery except: [:sample]
+  protect_from_forgery except: [:sample]
 
   def callback
     params = JSON.parse(request.body.read)
     Rails.logger.info params
-
     render nothing: true
   end
 end
