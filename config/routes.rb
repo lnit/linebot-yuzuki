7086ac327e85f252cd13f000c1642b3476dd24e7
lni_T <developer.lni@gmail.com>
2016-04-17 18:55:13 +0900

    getじゃなくてpostだった

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  get 'callback'=> 'linebots#callback'
+  post 'callback'=> 'linebots#callback'
 end
